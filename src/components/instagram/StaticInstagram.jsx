04b7943c4ce04807eb0f44151df7862eda4c71d2
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-10-26 13:21:56 +0200

    insta

--- src/components/instagram/StaticInstagram.jsx
+++ src/components/instagram/StaticInstagram.jsx
@@ -1,6 +1,22 @@
 "use client";
 import { useEffect } from "react";
 
+const instagramLinks = [
+  "https://www.instagram.com/reel/DBHmaFdCGkf/?utm_source=ig_embed&amp;utm_campaign=loading",
+  "https://www.instagram.com/reel/DBjvVYpBsCZ/?utm_source=ig_embed&amp;utm_campaign=loading",
+  "https://www.instagram.com/reel/DBes7pNCy9Q/?utm_source=ig_embed&amp;utm_campaign=loading",
+  "https://www.instagram.com/reel/DBZdZ1IC_Aa/?utm_source=ig_embed&amp;utm_campaign=loading",
+  "https://www.instagram.com/reel/DBWc41MSDJu/?utm_source=ig_embed&amp;utm_campaign=loading",
+  "https://www.instagram.com/reel/DBT--MQSRWY/?utm_source=ig_embed&amp;utm_campaign=loading",
+  "https://www.instagram.com/reel/DBLSZ1USWI7/?utm_source=ig_embed&amp;utm_campaign=loading",
+  "https://www.instagram.com/reel/C3c9gsRxDOg/?utm_source=ig_embed&amp;utm_campaign=loading",
+  "https://www.instagram.com/reel/DATm09ztoft/?utm_source=ig_embed&amp;utm_campaign=loading",
+  "https://www.instagram.com/reel/DAy8iPyiB0D/?utm_source=ig_embed&amp;utm_campaign=loading",
+  "https://www.instagram.com/reel/DA1CZqEyCwm/?utm_source=ig_embed&amp;utm_campaign=loading",
+  "https://www.instagram.com/reel/DAiUQm7R_PN/?utm_source=ig_embed&amp;utm_campaign=loading",
+  "https://www.instagram.com/reel/DAh4fqTioXz/?utm_source=ig_embed&amp;utm_campaign=loading",
+];
+
 const InstagramEmbed = () => {
   useEffect(() => {
     // Inject the Instagram embed script only once
@@ -32,7 +48,6 @@ const InstagramEmbed = () => {
           gap: 20px;
           padding: 20px;
           background-color: var(--teal-950);
-
           box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
           transition: background-color 0.3s ease;
         }
@@ -40,6 +55,7 @@ const InstagramEmbed = () => {
         .instagram-embed-container:hover {
           background-color: var(--teal-800);
         }
+
         .instagram-embed-container::-webkit-scrollbar {
           width: 10px;
         }
@@ -50,114 +66,27 @@ const InstagramEmbed = () => {
         }
       `}</style>
 
-      {/* Embed for the first Instagram post */}
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
-          <style>
-         .instagram-media {
-          max-height: 500px !important;
-         }
-         .instagram-media iframe {
-           margin: auto !important;
-         }
-         .instagram-media [role='button'], .instagram-media header,  .instagram-media .InstagramCaption,  {
-           display: none !important;
-         }
-      </style>
-                         <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="https://www.instagram.com/reel/DBHmaFdCGkf/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" ></blockquote></blockquote>
-            
-
-          `,
-        }}
-      />
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
-         
-                         <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="https://www.instagram.com/reel/DBes7pNCy9Q/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" ></blockquote>
-          `,
-        }}
-      />
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
-         
-                         <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="https://www.instagram.com/reel/DBZdZ1IC_Aa/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" ></blockquote></blockquote>
-          `,
-        }}
-      />
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
- <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="https://www.instagram.com/reel/DBWc41MSDJu/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" ></blockquote>          `,
-        }}
-      />
-
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
-          <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="https://www.instagram.com/reel/DBT--MQSRWY/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" ></blockquote>
-
-          `,
-        }}
-      />
-
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
-          <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="https://www.instagram.com/reel/DBLSZ1USWI7/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14"></blockquote>;
-
-          `,
-        }}
-      />
-
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
-             <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="https://www.instagram.com/reel/C3c9gsRxDOg/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14"></blockquote>
-          `,
-        }}
-      />
-
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
-            <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/reel/DATm09ztoft/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14"></blockquote>
-          `,
-        }}
-      />
-
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
-          <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="https://www.instagram.com/reel/DAy8iPyiB0D/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14"></blockquote> 
-        `,
-        }}
-      />
-
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
-          <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="https://www.instagram.com/reel/DA1CZqEyCwm/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14"></blockquote>
-        `,
-        }}
-      />
-
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
-            <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="https://www.instagram.com/reel/DAiUQm7R_PN/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" ></blockquote>
-        `,
-        }}
-      />
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `
-            <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="https://www.instagram.com/reel/DAh4fqTioXz/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14"></blockquote>
-        `,
-        }}
-      />
+      {instagramLinks.map((link, index) => (
+        <div
+          key={index}
+          dangerouslySetInnerHTML={{
+            __html: `
+              <style>
+                .instagram-media {
+                  max-height: 500px !important;
+                }
+                .instagram-media iframe {
+                  margin: auto !important;
+                }
+                .instagram-media [role='button'], .instagram-media header, .instagram-media .InstagramCaption {
+                  display: none !important;
+                }
+              </style>
+              <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="${link}" data-instgrm-version="14"></blockquote>
+            `,
+          }}
+        />
+      ))}
     </div>
   );
 };
