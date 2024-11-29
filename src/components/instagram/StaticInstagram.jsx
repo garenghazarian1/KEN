6b420c4814ba5678e36714b5e82c8d38b4da88a9
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2024-11-29 20:09:45 +0100

    insta 29 nov 2024

--- src/components/instagram/StaticInstagram.jsx
+++ src/components/instagram/StaticInstagram.jsx
@@ -2,6 +2,8 @@
 import { useEffect } from "react";
 
 const instagramLinks = [
+  "https://www.instagram.com/reel/DC9dIVSBLb3/?utm_source=ig_embed&amp;utm_campaign=loading",
+  "https://www.instagram.com/reel/DC4egODhnCv/?utm_source=ig_embed&amp;utm_campaign=loading",
   "https://www.instagram.com/reel/DCCAjjsyBZW/?utm_source=ig_embed&amp;utm_campaign=loading",
   "https://www.instagram.com/reel/DC1qHLQSFen/?utm_source=ig_embed&amp;utm_campaign=loading",
   "https://www.instagram.com/reel/DCzWY26BTRG/?utm_source=ig_embed&amp;utm_campaign=loading",
